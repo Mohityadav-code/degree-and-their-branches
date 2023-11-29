8a8212cc300d882ce1fccb7962095c65a9aa9b35
Mohit  Yadav <79302660+Mohityadav-code@users.noreply.github.com>
2023-11-29 23:15:55 +0530

    Create categories.js

---
+++ categories.js
@@ -0,0 +1,16 @@
+const categories = [
+    { abbreviation: 'BC', name: 'Backward Classes' },
+    { abbreviation: 'MBC', name: 'Most Backward Classes' },
+    { abbreviation: 'DC', name: 'Denotified Communities' },
+    { abbreviation: 'OBC', name: 'Other than Backward Class' },
+    { abbreviation: 'OBCM', name: 'Other than Backward Class Muslims' },
+    { abbreviation: 'ST', name: 'Scheduled Tribes' },
+    { abbreviation: 'SC', name: 'Scheduled Castes' },
+    { abbreviation: 'SC(A)', name: 'Scheduled Castes (Arunthathiyars)' },
+    { abbreviation: 'G', name: 'General' },
+    { abbreviation: 'W', name: 'Women' },
+    { abbreviation: 'PSTM', name: 'Persons Studied in Tamil Medium' },
+    { abbreviation: 'GT', name: 'General Turn' }
+];
+
+console.log(categories);
